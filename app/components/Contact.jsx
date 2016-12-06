66bfc8e69896e2bec36e692ef6aa616fc4cc7dde
Tyler Schmidt <schmidt197@gmail.com>
2016-12-06 12:49:15 -0800

    add grid system and styles for contact

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -3,8 +3,18 @@ var React = require('react');
 var Contact = (props) => {
   return (
     <div>
-      <h1>Contact</h1>
-      </div>
+      <h1 className="text-center">Contact</h1>
+      <ul className="text-center">
+        <li>
+          <p>3440 20th st. #105</p>
+          <p>San Francisco, CA 94110</p>
+        </li>
+        <li>
+          <p>Phone:</p>
+          <p>451-812-1805</p>
+        </li>
+      </ul>
+    </div>
   );
 }
 
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,14 +3,36 @@ var {Link, IndexLink} = require('react-router');
 
 var Nav = (props) => {
   return (
-<div>
-    <h2>Nav Component</h2>
-    <IndexLink to="/" activeClassName="active">Pylon</IndexLink>
-    <Link to="/about" activeClassName="active">About</Link>
-    <Link to="/contact" activeClassName="active">Contact</Link>
-</div>
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li>
+            <IndexLink to="/" activeClassName="active" className="menu-text">Pylon</IndexLink>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li>
+            <Link to="/about" activeClassName="active">About</Link>
+            </li>
+            <li>
+            <Link to="/contact" activeClassName="active">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
     );
   }
 
 
 module.exports = Nav;
+
+var old = (
+<div>
+  <h2>Nav Component</h2>
+  <IndexLink to="/" activeClassName="active">Pylon</IndexLink>
+  <Link to="/about" activeClassName="active">About</Link>
+  <Link to="/contact" activeClassName="active">Contact</Link>
+</div>
+)
