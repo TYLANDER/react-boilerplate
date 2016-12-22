acb782980529f2078605312dea8da31c1e47033c
Tyler Schmidt <schmidt197@gmail.com>
2016-12-21 17:14:15 -0800

    styled contact page. ready to deploy

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -2,17 +2,17 @@ var React = require('react');
 
 var Contact = (props) => {
   return (
-    <div>
-    <div className='row align-center'>
-      <h2>New Business Inquiries</h2>
+
+      <div className="wrapper">
+        <div className='row align-center'>
+          <h1 id="new-biz-text">New Business Inquiries</h1>
+        </div>
+
+        <div className='row align-center'>
+          <a id="contactlink" href="mailto:tyler@pylon.design">Tyler Schmidt</a>
+        </div>
     </div>
-    <div className='row align-center'>
-      <p>Contact:</p>
-    </div>
-    <div className='row align-center'>
-      <a href="mailto:tyler@pylon.design">Tyler Schmidt</a>
-    </div>
-  </div>
+
   );
 }
 
