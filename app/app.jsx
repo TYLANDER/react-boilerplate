56590dc7dfbb43e32af69ab0f012d517f34123b8
Tyler Schmidt <schmidt197@gmail.com>
2017-01-09 13:04:17 -0800

    fixed bug replacing hashHistory w browserBistory in app.jsx react router

--- app/app.jsx
+++ app/app.jsx
@@ -1,12 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {Route, Router, IndexRoute} = require('react-router');
+var {browserHistory} = require('react-router');
 var Main = require('Main');
 var Pylon = require('Pylon');
 var About = require('About');
 var Contact = require('Contact');
 var Logo = require('Logo');
-
 
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css')
@@ -16,7 +16,7 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={Main}>
       <IndexRoute component={Pylon}/>
       <Route path="about" component={About}/>
